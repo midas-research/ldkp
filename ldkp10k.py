0382ed9175b042c64ea03e41e8d5874563cada88
Navneet Agarwal <53942457+navneet-ag@users.noreply.github.com>
2021-08-22 20:17:56 +0530

    Added download files

---
+++ ldkp10k.py
@@ -0,0 +1,16 @@
+from helper import *
+
+#Use the below code if you want to download the LDKP3K dataset in base format
+# ldkp_10k_base_test()
+# ldkp_10k_base_val()
+# ldkp_10k_base_small_train()
+# ldkp_10k_base_medium_train()
+# ldkp_10k_base_large_train()
+
+#Use the below code if you want to download the LDKP3K dataset in CoNLL format
+
+ldkp_10k_conll_test()
+ldkp_10k_conll_val()
+ldkp_10k_conll_small_train()
+ldkp_10k_conll_medium_train()
+ldkp_10k_conll_large_train()
